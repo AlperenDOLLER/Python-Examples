338bc8dda5afb72634e5ff366a8899096aee0447
alperen2626 <71827182+alperen2626@users.noreply.github.com>
2020-09-24 22:12:52 +0300

    Create Variables_String_fstring.py

---
+++ Variables_String_fstring.py
@@ -0,0 +1,17 @@
+
+my_string="Alperen's first example "
+
+#We add the first string to the second string
+
+my_another_string="Hello world "+my_string
+
+""""
+*The above line can also done by using f'str'comment : my_another_string=f"Hello world {my_string}"
+
+*The above line can also done by using print(my_another_string.format(my_string)) 
+but second string must be configured my_another_string="Hello world {}"
+
+"""
+
+print(my_another_string)
+
