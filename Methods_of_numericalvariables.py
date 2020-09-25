240ae11611521db47f714b3d90cc0f58efc8d653
alperen2626 <71827182+alperen2626@users.noreply.github.com>
2020-09-25 16:57:34 +0300

    Create Methods_of_numericalvariables.py
    The methods and mapulation of the numerical variables

---
+++ Methods_of_numericalvariables.py
@@ -0,0 +1,39 @@
+number1=-5
+
+print(number1)
+
+#Taking absolute value of the number
+print(abs(number1))
+
+#Rounding the nearest integer
+number2=6.48
+print(round(number2))
+
+number3=6.75
+print(round(number3))
+
+#The power of the number
+
+print(pow(3,2))
+
+#Other manupulation on numbers
+
+number1 +=2
+print(number1)
+
+number1 *=2
+print(number1)
+
+s1="15"
+s2="26"
+
+s1=int(s1)
+s2=int(s2)
+
+print(s1+s2)
+
+#To write the string x times print(srtin*x)
+
+my_string="test "
+
+print(my_string*3)
