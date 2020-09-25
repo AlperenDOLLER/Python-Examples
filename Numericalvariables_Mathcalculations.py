fdd161e62516f59075f6f3e289b488474e0d0e9c
alperen2626 <71827182+alperen2626@users.noreply.github.com>
2020-09-25 16:57:06 +0300

    Create Numericalvariables_Mathcalculations.py

---
+++ Numericalvariables_Mathcalculations.py
@@ -0,0 +1,30 @@
+x=26
+
+#Type of the variable x 
+print(type(x))
+
+y=26.5
+print(type(y))
+
+#If the variable inside of quotes python take the variable type as a string
+s="26"
+print(type(s))
+
+
+"""
+Mathematical calculations
+Multiplication=5*3
+Division=8/2
+Integer Division=9//2
+Power=3**3
+Mod of the number=102%5
+
+"""
+print(5+3)
+print(3-5)
+print(5*3)
+print(5/3)
+print(8/2)
+print(9//2)
+print(3**3)
+print(102%5)
